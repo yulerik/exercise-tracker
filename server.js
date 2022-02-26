20181e8100eb3d0ecf97977cd1139c62303edcbc
Yulerik Sorensen <yulerik.sorensen@gmail.com>
2022-02-25 21:26:57 -0800

    updated for heroku deployment

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const expressJwt = require('express-jwt')
 const port = process.env.PORT || 9000
 const uri = process.env.MONGODB_URI
 const path = require('path')
+const secret = process.env.SECRET || 'local deployment phrase'
 
 app.use(express.json())
 app.use(morgan('dev'))
@@ -24,7 +25,7 @@ mongoose.connect(
 )
 
 app.use('/auth', require('./routes/authRouter.js'))
-app.use('/api', expressJwt({ secret: process.env.SECRET,  algorithms: ['HS256'] })) // req.user
+app.use('/api', expressJwt({ secret: secret,  algorithms: ['HS256'] })) // req.user
 // app.use('/api/user', require('./routes/userRouter.js'))
 app.use('/api/exercise', require('./routes/exerciseRouter'))
 app.use('/api/workout', require('./routes/workoutRouter'))
